c0bb308df9641a9d723818e1d1de1005b3917633
tiffany <tiffany940407@yahoo.com.tw>
2024-05-01 18:38:55 +0800

    feat(ts): 利用陣列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10),
-  undefined
-);
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+  new Product(
+    2,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+];
 
-console.log(product);
-console.table(product);
+console.log(products);
+console.table(products);
