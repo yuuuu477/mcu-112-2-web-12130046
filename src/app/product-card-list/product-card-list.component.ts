c7ea52d49d42b4e642480496660b8f54dcc1b478
tiffany <tiffany940407@yahoo.com.tw>
2024-05-01 21:08:43 +0800

    feat(product): 建立產品資料模型

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,23 @@
-isShow = true;
-imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-createDate = new Date();
-price = 10000;
+import { Component } from '@angular/core';
+import { Product } from '../model/product';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'app-product-card-list',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './product-card-list.component.html',
+  styleUrl: './product-card-list.component.css',
+})
+export class ProductCardListComponent {
+  product = new Product({
+    id: 1,
+    name: '書籍 A',
+    authors: '作者甲、作者乙、作者丙',
+    company: '博碩文件',
+    isShow: true,
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    createDate: new Date(),
+    price: 10000,
+  });
 }
