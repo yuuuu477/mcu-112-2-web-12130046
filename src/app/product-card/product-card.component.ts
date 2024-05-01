c63c30d29a39790ec87542b284d7caa425b5af75
tiffany <tiffany940407@yahoo.com.tw>
2024-05-01 20:20:14 +0800

    feat(product): 利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-product-card',
   standalone: true,
@@ -12,6 +11,7 @@ export class ProductCardComponent {
   authors = '作者甲、作者乙、作者丙';
   company = '博碩文件';
   isShow = true;
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
