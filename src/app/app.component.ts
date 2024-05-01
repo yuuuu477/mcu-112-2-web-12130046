8fe54d1ba8b8c2df254ce4ddbc346862be106808
tiffany <tiffany940407@yahoo.com.tw>
2024-05-01 23:24:05 +0800

    feat(header): 新增標題元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+
 import { Product } from './model/product';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardListComponent],
+  imports: [HeaderComponent, ProductCardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
