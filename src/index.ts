ef486fb59f4e30ad874f037e8a48196fd15a1ba0
tiffany <tiffany940407@yahoo.com.tw>
2024-05-01 17:58:59 +0800

    feat(ts):利用物件紀錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-let productId = 1;
-console.log("Product Id = ", productId);
-
-const productName = "圖像 Algular 開發入門 第二版";
-console.log("Product Name = ", productName);
-
-let isShow = true;
-console.log("Is Show = ", isShow);
-
-let createDate = new Date();
-console.log("Create Date = ", createDate);
-
-let modifyDate: Date | undefined = undefined;
-console.log("Modify Date", modifyDate);
+const product = {
+  id: 1,
+  name: "圖像 Angular 開發入門 第二版",
+  company: "博碩文化",
+  price: 500,
+  isShow: true,
+  createDate: new Date(2024, 3, 10),
+  modifyDate: undefined,
+};
+console.log(product);
+console.table(product);
