22b80ea35e2a992b20fd7b5628c8e6f7431d9fad
tiffany <chihyuu2547>
2024-05-08 17:52:18 +0800

    feat(product-detail): 實作在產品明細頁刪除產品資料

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit, inject, numberAttribute } from '@angular/core
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductService } from '../services/product.service';
-
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
@@ -14,20 +13,21 @@ import { ProductService } from '../services/product.service';
 export class ProductDetailPageComponent implements OnInit {
   @Input({ transform: numberAttribute })
   id!: number;
-
   product!: Product;
-
   private router = inject(Router);
-
   private productService = inject(ProductService);
-
   ngOnInit(): void {
     this.product = this.productService.getById(this.id);
   }
-
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product.id]);
   }
+
+  onRemove(): void {
+    this.productService.remove(this.product.id);
+    this.router.navigate(['products']);
+  }
+
   onBack(): void {
     this.router.navigate(['products']);
   }
