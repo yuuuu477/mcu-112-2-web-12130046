01d9365090f91d3118c28eddf244be7371fc3939
tiffany <chihyuu2547>
2024-05-08 17:45:05 +0800

    feat(product-detail): 產品檢視窗頁面切版實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css',
 })
-export class ProductDetailPageComponent {}
+export class ProductDetailPageComponent {
+  product = new Product({
+    id: 1,
+    name: '書籍 A',
+    authors: ['作者甲', '作者乙', '作者丙'],
+    company: '博碩文件',
+    isShow: true,
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    createDate: new Date(),
+    price: 10000,
+  });
+
+  private router = inject(Router);
+
+  onEdit(): void {
+    this.router.navigate(['product', 'form', this.product.id]);
+  }
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
+}
