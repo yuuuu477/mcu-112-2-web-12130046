bbb0e747f03ea748bc90824bcc15041a2bab70a3
tiffany <chihyuu2547>
2024-05-15 17:53:17 +0800

    feat(resolver): 建立產品資料預戴資料

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductFormPageComponent } from './product-form-page/product-form-page.component';
+import { productResolver } from './resolver/product.resolver';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
-  { path: 'product/view/:id', component: ProductDetailPageComponent },
+  { path: 'product/view/:id', component: ProductDetailPageComponent, resolve: { product: productResolver } },
   { path: 'product/form/:id', component: ProductFormPageComponent },
   { path: 'login', component: LoginPageComponent },
 ];
--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input, OnInit, inject, numberAttribute } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductService } from '../services/product.service';
@@ -10,15 +10,12 @@ import { ProductService } from '../services/product.service';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css',
 })
-export class ProductDetailPageComponent implements OnInit {
-  @Input({ transform: numberAttribute })
-  id!: number;
+export class ProductDetailPageComponent {
+  @Input()
   product!: Product;
   private router = inject(Router);
   private productService = inject(ProductService);
-  ngOnInit(): void {
-    this.productService.getById(this.id).subscribe((product) => (this.product = product));
-  }
+
   onEdit(): void {
     this.router.navigate(['product', 'form', this.product.id]);
   }
