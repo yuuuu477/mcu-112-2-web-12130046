4fac6d6ac23825126bcf400ead0e963ef7dcb227
tiffany <chihyuu2547>
2024-05-22 17:56:15 +0800

    feat(product-detail): 實作從後端服務取得產品明細資料

--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -12,6 +12,10 @@ export class ProductRemoteService extends ProductService {
 
   private readonly httpClient = inject(HttpClient);
 
+  override getById(productId: number): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.url}/${productId}`);
+  }
+
   override getList(): Observable<Product[]> {
     return this.httpClient.get<Product[]>(this.url);
   }
