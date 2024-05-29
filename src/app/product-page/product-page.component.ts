3471105a4dd018bf17470789639c77096bbbb2d7
tiffany <chihyuu2547>
2024-05-29 18:19:55 +0800

    feat(product): 查詢列表單設定

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,14 +1,16 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from '../services/product.service';
+
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [AsyncPipe, ProductCardListComponent],
+  imports: [AsyncPipe, ReactiveFormsModule, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
@@ -17,9 +19,10 @@ export class ProductPageComponent {
   private productService = inject(ProductService);
 
   private readonly refresh$ = new Subject<void>();
+  protected readonly formControl = new FormControl<string | undefined>(undefined);
   readonly products$ = this.refresh$.pipe(
     startWith(undefined),
-    switchMap(() => this.productService.getList('書籍B', 1, 5))
+    switchMap(() => this.productService.getList(undefined, 1, 5))
   );
 
   onAdd(): void {
--- src/app/services/product-remote.service.ts
+++ src/app/services/product-remote.service.ts
@@ -19,6 +19,7 @@ export class ProductRemoteService extends ProductService {
   override getList(name: string | undefined, pageIndex: number, pageSize: number): Observable<Product[]> {
     const query: { [key: string]: string | number } = { _page: pageIndex, _limit: pageSize };
     if (name !== undefined) query['name'] = name;
+    ``;
     const params = new HttpParams({ fromObject: query });
     return this.httpClient.get<Product[]>(this.url, { params });
   }
