dfde35bb2f0b0d4af641bf159717e2b55e1c6c8b
tiffany <chihyuu2547>
2024-06-05 17:55:57 +0800

    feat(product-form): 定義多筆作者輸入

--- src/app/interface/product-form.interface.ts
+++ src/app/interface/product-form.interface.ts
@@ -1,8 +1,9 @@
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl } from '@angular/forms';
 
 export interface IProductForm {
   id: FormControl<number | null>;
   name: FormControl<string | null>;
+  authors: FormArray<FormControl<string | null>>;
   company: FormControl<string | null>;
   isShow: FormControl<boolean>;
   price: FormControl<string | null>;
--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { IProductForm } from '../interface/product-form.interface';
@@ -19,13 +19,24 @@ export class ProductFormPageComponent implements OnInit {
   form = new FormGroup<IProductForm>({
     id: new FormControl<number | null>(null),
     name: new FormControl<string | null>(null),
+    authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
     price: new FormControl<string | null>(null),
   });
+
+  get authors(): FormArray<FormControl<string | null>> {
+    return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
   product!: Product;
 
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthors(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
   }
 }
