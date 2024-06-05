07845bdee92e6ee8363a73de6a4a2a67ec44708c
tiffany <chihyuu2547>
2024-06-05 18:11:32 +0800

    feat(product-form): 產品表單驗證實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,11 +1,10 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { IProductForm } from '../interface/product-form.interface';
 import { Product } from '../model/product';
-
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
@@ -18,15 +17,27 @@ export class ProductFormPageComponent implements OnInit {
 
   form = new FormGroup<IProductForm>({
     id: new FormControl<number | null>(null),
-    name: new FormControl<string | null>(null),
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     authors: new FormArray<FormControl<string | null>>([]),
-    company: new FormControl<string | null>(null),
+    company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     isShow: new FormControl<boolean>(false, { nonNullable: true }),
-    price: new FormControl<string | null>(null),
+    price: new FormControl<string | null>(null, { validators: [Validators.required, Validators.pattern('^[0-9]*$')] }),
   });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
 
   get authors(): FormArray<FormControl<string | null>> {
     return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
+  get company(): FormControl<string | null> {
+    return this.form.get('company') as FormControl<string | null>;
+  }
+
+  get price(): FormControl<string | null> {
+    return this.form.get('price') as FormControl<string | null>;
   }
 
   product!: Product;
@@ -36,7 +47,7 @@ export class ProductFormPageComponent implements OnInit {
   }
 
   onAddAuthors(): void {
-    const formControl = new FormControl<string | null>(null);
+    const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
     this.authors.push(formControl);
   }
 }
